ecc5c6e67fc72dccc3ee70ba2e1df29bbdfb5d9e
Brandon Fowler <bmfowler4@gmail.com>
2018-01-02 16:15:40 -1000

    finished exercises

--- vault.js
+++ vault.js
@@ -1,4 +1,19 @@
 'use strict';
 module.exports = function() {
-  
+    const vault = {};
+
+    function setValue(key, value) {
+        vault[key] = value;
+    }
+
+    function getValue(key) {
+        if (vault.hasOwnProperty(key))
+            return vault[key];
+        return null;
+    }
+
+    return {
+        setValue: setValue,
+        getValue: getValue
+    }
 };
